587b781d501345e137b535d0e2051cd4d51bab3a
ShHaWkK <alexandreuzan9@gmail.com>
2024-03-19 23:37:45 +0100

    ADD Folder balade

---
+++ site/balade/balade.php
@@ -0,0 +1,138 @@
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+<section class="pageTitle">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="pageTitleInfo">
+          <h1>Balades</h1>
+          <div class="under-border"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="media packagesList thumbnail deals">
+  <a class="media-left fancybox-pop" href="https://reservation.canoe-kayak-dordogne.com/storage/balades/images/DQnEq-084244-COX.jpeg">
+    <img class="media-object" src="https://reservation.canoe-kayak-dordogne.com/storage/balades/images/DQnEq-084244-COX.jpeg" alt="Beaulieu-sur-Dordogne - Saint-Sozy">
+  </a>
+                                    
+  <div class="media-body">
+    <div class="bodyLeft">
+      <h4 class="media-heading"><a href="https://reservation.canoe-kayak-dordogne.com/detail-parcours/15-beaulieu-sur-dordogne-saint-sozy">Beaulieu-sur-Dordogne - Saint-Sozy</a></h4>
+      <div class="countryRating">
+        <span>Séjour itinérant</span>
+      </div>
+      <br>
+      <p>Description de la balade</p>
+      <span class="textInfo"><i class="fa fa-clock-o" aria-hidden="true"></i> 2 jours</span>
+    </div>
+    <div class="bodyRight">
+      <div class="bookingDetails">
+        <p>à partir de</p>
+        <h2>52 €</h2>
+        <p>par personne</p>
+        <a href="https://reservation.canoe-kayak-dordogne.com/detail-parcours/15-beaulieu-sur-dordogne-saint-sozy" class="btn buttonTransparent clearfix">Réserver</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+</section>
+<style>
+ /* Styles pour la section de la page */
+.pageTitle {
+  background-color: #f5f5f5;
+  padding: 20px;
+  text-align: center;
+}
+
+.pageTitle h1 {
+  font-size: 36px;
+  margin-bottom: 10px;
+}
+
+.under-border {
+  width: 100px;
+  height: 3px;
+  background-color: #333;
+  margin: 0 auto;
+}
+
+/* Styles pour la liste des balades */
+/* Définition des styles pour .sidebarPage .packagesList */
+.sidebarPage .packagesList {
+  min-height: 234px;
+}
+
+/* Réinitialisation des styles pour tous les éléments */
+* {
+  outline: 0;
+  margin: 0;
+  padding: 0;
+}
+
+/* Styles pour .bodyLeft et .bodyRight */
+.bodyLeft, .bodyRight {
+  display: inline-block;
+  width: 70%;
+}
+
+/* Styles pour les images dans .packagesList .media-left */
+.packagesList .media-left img {
+  width: 100%;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+/* Styles spécifiques pour .packagesList .media-left img à partir de 992px */
+@media (min-width: 992px) {
+  .packagesList .media-left img {
+    border-top-right-radius: 0px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
+}
+
+/* Styles pour .packagesList .media-body */
+.packagesList .media-body {
+  position: relative;
+  height: 100%;
+  overflow: visible;
+}
+
+/* Styles pour les images dans .thumbnail a et .thumbnail */
+.thumbnail a > img, .thumbnail > img {
+  margin-right: auto;
+  margin-left: auto;
+}
+
+
+</style>
