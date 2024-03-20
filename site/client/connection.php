021f6ace3d4fdadea1f014c89b68ecb0f4794e9e
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2024-03-20 20:53:18 +0100

    Update connection.php

--- site/client/connection.php
+++ site/client/connection.php
@@ -1,9 +1,14 @@
 <?php
-try 
-{
-	$conn = new PDO('mysql:host=51.77.157.224;dbname=river_ride', 'prisk', 'prisca', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-}
-catch(PDOException $e){
-	die('Erreur : ' . $e->getMessage());
+
+$servername = "";
+$dbname = ""; 
+$username = "";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connexion échouée : " . $e->getMessage();
 }
 ?>
