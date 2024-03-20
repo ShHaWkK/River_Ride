fa95d9b597f43035d7acd0e4d90631b21e8eb779
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2024-03-20 23:26:29 +0100

    Update Connection.php

--- site/reservation/Connection.php
+++ site/reservation/Connection.php
@@ -3,7 +3,7 @@
 
 try 
 {
-	$conn = new PDO('mysql:host=51.77.157.224;dbname=gymlight', 'prisk', 'prisca', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+	$conn = new PDO('mysql:host=;dbname=gymlight', '', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
 catch(PDOException $e){
 	die('Erreur : ' . $e->getMessage());
